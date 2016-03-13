92f4f81c823f799a7fee6dccc50a834f7960458c
Carsten Gräf <evandor@gmail.com>
2016-03-13 16:37:31 +0100

    initial app

---
+++ skysail.product.timetables/src/io/skysail/product/timetables/TimetablesRootApplication.java
@@ -0,0 +1,27 @@
+package io.skysail.product.timetables;
+
+import org.osgi.service.event.EventAdmin;
+
+import de.twenty11.skysail.server.core.restlet.RouteBuilder;
+import io.skysail.server.app.ApiVersion;
+import io.skysail.server.app.ApplicationProvider;
+import io.skysail.server.app.SkysailApplication;
+
+public class TimetablesRootApplication extends SkysailApplication implements ApplicationProvider {
+
+    public TimetablesRootApplication() {
+        super("timetables", new ApiVersion(1));
+    }
+    
+    @Override
+    public EventAdmin getEventAdmin() {
+        return null;
+    }
+
+    @Override
+    protected void attach() {
+        router.setApiVersion(null);
+        router.attach(new RouteBuilder("", PublicResource.class).noAuthenticationNeeded());
+    }
+}
+
