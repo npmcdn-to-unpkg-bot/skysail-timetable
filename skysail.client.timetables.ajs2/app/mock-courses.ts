8d7465c2f215e396431372d8cded4d1b1d45f2b5
Carsten Gräf <evandor@gmail.com>
2016-03-20 13:15:27 +0100

    wip

--- skysail.client.timetables.ajs2/app/mock-courses.ts
+++ skysail.client.timetables.ajs2/app/mock-courses.ts
@@ -2,5 +2,6 @@ import { Course } from './course';
 
 export var COURSES: Course[] = [
 	{"id": 11, "name": "ZUMBA 01", "room": "E2", "trainer": "Ricky"},
-	{"id": 12, "name": "ZUMBA 02", "room": "E3", "trainer": "Ricky"}
+	{"id": 12, "name": "ZUMBA 02", "room": "E3", "trainer": "Ricky"},
+	{"id": 13, "name": "ZUMBA 03", "room": "E3", "trainer": "Ricky"}
 ];
