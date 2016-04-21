4f29180902997ee5fee5db12f662545700705264
Carsten Gräf <evandor@gmail.com>
2016-04-21 07:13:10 +0200

    wip

--- skysail.server.app.timetables/client/client/components/app.ts
+++ skysail.server.app.timetables/client/client/components/app.ts
@@ -3,10 +3,12 @@ import {RouteConfig, Router, RouterOutlet, RouterLink} from 'angular2/router';
 
 import {Dashboard} from './dashboard';
 import {Manage} from './manage';
+import {Home} from './home';
 
 @RouteConfig([
   {path: '/', as: 'Dashboard', component: Dashboard},
-  {path: '/manage', as: 'Manage', component: Manage}
+  {path: '/manage', as: 'Manage', component: Manage},
+  {path: '/home', as: 'Home', component: Home}
 ])
 
 @Component({
@@ -30,37 +32,26 @@ import {Manage} from './manage';
       <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
               <li><a href="#myLogin" data-toggle="modal">Anmelden</a></li>
-              <li><a href="/courses">[Angemeldet]</a></li>
-              <li><a href="/dashboard">[Abgemeldet]</a></li>
           </ul>
       </div>
     </div>
   </nav>
 
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Stock Tracker!</span>
-        <div class="mdl-layout-spacer"></div>
-        <nav class="mdl-navigation mdl-layout--large-screen-only">
-          <a class="mdl-navigation__link" [router-link]="['/Dashboard']">Dashboard</a>
-          <a class="mdl-navigation__link" [router-link]="['/Manage']">Manage</a>
-        </nav>
-      </div>
-    </header>
-    <main class="mdl-layout__content" style="padding: 20px;">
-      <router-outlet></router-outlet>
-    </main>
-  </div>
+  <main class="mdl-layout__content" style="padding: 20px;">
+    <router-outlet></router-outlet>
+  </main>
 
+<hr>
   <hr>
   <div class="container">
     <div class="myfooter">
-      <footer>Copyright &copy; skysail.io 2016</footer>
+      <footer>Copyright &copy; skysail.io 2016...
+        <a class="mdl-navigation__link" [router-link]="['/Dashboard']">Dashboard</a>
+        <a class="mdl-navigation__link" [router-link]="['/Manage']">Manage</a>
+        <a class="mdl-navigation__link" [router-link]="['/Home']">Home</a>
+      </footer>
     </div>
   </div>
-
-
   `
 })
 export class App {
--- skysail.server.app.timetables/client/dist/client/components/app.js
+++ skysail.server.app.timetables/client/dist/client/components/app.js
@@ -13,20 +13,22 @@ var angular2_1 = require('angular2/angular2');
 var router_1 = require('angular2/router');
 var dashboard_1 = require('./dashboard');
 var manage_1 = require('./manage');
+var home_1 = require('./home');
 var App = (function () {
     function App() {
     }
     App = __decorate([
         router_1.RouteConfig([
             { path: '/', as: 'Dashboard', component: dashboard_1.Dashboard },
-            { path: '/manage', as: 'Manage', component: manage_1.Manage }
+            { path: '/manage', as: 'Manage', component: manage_1.Manage },
+            { path: '/home', as: 'Home', component: home_1.Home }
         ]),
         angular2_1.Component({
             selector: 'app'
         }),
         angular2_1.View({
             directives: [router_1.RouterOutlet, router_1.RouterLink],
-            template: "\n  <nav class=\"navbar navbar-default navbar-fixed-top\">\n    <div class=\"container\">\n      <div class=\"navbar-header\">\n          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">\n              <span class=\"sr-only\">Toggle navigation</span>\n              <span class=\"icon-bar\"></span>\n              <span class=\"icon-bar\"></span>\n              <span class=\"icon-bar\"></span>\n          </button>\n          <a class=\"navbar-brand\" href=\"/\"><span class=\"glyphicon glyphicon-calendar green\" aria-hidden=\"true\"></span> skysail kursplan</a>\n\n      </div>\n      <div id=\"navbar\" class=\"navbar-collapse collapse\">\n          <ul class=\"nav navbar-nav navbar-right\">\n              <li><a href=\"#myLogin\" data-toggle=\"modal\">Anmelden</a></li>\n              <li><a href=\"/courses\">[Angemeldet]</a></li>\n              <li><a href=\"/dashboard\">[Abgemeldet]</a></li>\n          </ul>\n      </div>\n    </div>\n  </nav>\n\n  <div class=\"mdl-layout mdl-js-layout mdl-layout--fixed-header\">\n    <header class=\"mdl-layout__header\">\n      <div class=\"mdl-layout__header-row\">\n        <span class=\"mdl-layout-title\">Stock Tracker!</span>\n        <div class=\"mdl-layout-spacer\"></div>\n        <nav class=\"mdl-navigation mdl-layout--large-screen-only\">\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Dashboard']\">Dashboard</a>\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Manage']\">Manage</a>\n        </nav>\n      </div>\n    </header>\n    <main class=\"mdl-layout__content\" style=\"padding: 20px;\">\n      <router-outlet></router-outlet>\n    </main>\n  </div>\n\n  <hr>\n  <div class=\"container\">\n    <div class=\"myfooter\">\n      <footer>Copyright &copy; skysail.io 2016</footer>\n    </div>\n  </div>\n\n\n  "
+            template: "\n  <nav class=\"navbar navbar-default navbar-fixed-top\">\n    <div class=\"container\">\n      <div class=\"navbar-header\">\n          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">\n              <span class=\"sr-only\">Toggle navigation</span>\n              <span class=\"icon-bar\"></span>\n              <span class=\"icon-bar\"></span>\n              <span class=\"icon-bar\"></span>\n          </button>\n          <a class=\"navbar-brand\" href=\"/\"><span class=\"glyphicon glyphicon-calendar green\" aria-hidden=\"true\"></span> skysail kursplan</a>\n\n      </div>\n      <div id=\"navbar\" class=\"navbar-collapse collapse\">\n          <ul class=\"nav navbar-nav navbar-right\">\n              <li><a href=\"#myLogin\" data-toggle=\"modal\">Anmelden</a></li>\n          </ul>\n      </div>\n    </div>\n  </nav>\n\n  <main class=\"mdl-layout__content\" style=\"padding: 20px;\">\n    <router-outlet></router-outlet>\n  </main>\n\n<hr>\n  <hr>\n  <div class=\"container\">\n    <div class=\"myfooter\">\n      <footer>Copyright &copy; skysail.io 2016...\n        <a class=\"mdl-navigation__link\" [router-link]=\"['/Dashboard']\">Dashboard</a>\n        <a class=\"mdl-navigation__link\" [router-link]=\"['/Manage']\">Manage</a>\n        <a class=\"mdl-navigation__link\" [router-link]=\"['/Home']\">Home</a>\n      </footer>\n    </div>\n  </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], App);
